afb7bb11f6909ddb2a906df5ccc6ab09b2ed468c
shubhampatil01 <89505897+shubhampatil01@users.noreply.github.com>
2023-02-28 12:52:39 +0530

    Delete OneDrive/Desktop/Ecommerce Website directory

--- OneDrive/Desktop/Ecommerce Website/src/index.js
+++
@@ -1,8 +0,0 @@
-import ReactDOM from "react-dom/client";
-import "../node_modules/bootstrap/dist/js/bootstrap";
-import "../node_modules/bootstrap/dist/css/bootstrap.css";
-import "./index.css";
-import App from "./App";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
