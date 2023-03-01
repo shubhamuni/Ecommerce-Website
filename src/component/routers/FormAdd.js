89b7e23bdff0eae9e1e6c18c6d641aa38b7662ac
shubhampatil.ani@gmail.com <shubhampatil.ani@gmail.com>
2023-03-01 15:44:50 +0530

    clear

--- src/component/routers/FormAdd.js
+++ src/component/routers/FormAdd.js
@@ -1,34 +1,67 @@
-import { Fragment, useRef } from "react";
+import { Fragment, useRef, useState } from "react";
 import { Button, Form } from "react-bootstrap";
 
 const FormAdd = (props) => {
+  const [inputValues, setInputValues] = useState({
+    name: "",
+    email: "",
+    phone: "",
+  });
   const nameRef = useRef();
   const emailRef = useRef();
   const phoneRef = useRef();
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setInputValues((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
   const submitHandler = (event) => {
     event.preventDefault();
 
     const form = {
       name: nameRef.current.value,
       email: emailRef.current.value,
-      phoneRef: phoneRef.current.value,
+      phone: phoneRef.current.value,
     };
     props.onAddForm(form);
+    setInputValues({
+      name: "",
+      email: "",
+      phone: "",
+    });
   };
 
   return (
     <Fragment>
-      <Form className="m-5" onSubmit={submitHandler}>
+      <Form className="m-5" onSubmit={submitHandler} id="myform">
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Name</Form.Label>
-          <Form.Control type="text" placeholder="Enter Name" ref={nameRef} />
+          <Form.Control
+            type="text"
+            placeholder="Enter Name"
+            ref={nameRef}
+            name="name"
+            value={inputValues.name}
+            onChange={handleInputChange}
+          />
           <Form.Text className="text-muted">
             Please Enter Your Full Name Here.
           </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" ref={emailRef} />
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            ref={emailRef}
+            name="email"
+            value={inputValues.email}
+            onChange={handleInputChange}
+          />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
@@ -36,7 +69,14 @@ const FormAdd = (props) => {
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Phone Number</Form.Label>
-          <Form.Control type="tel" placeholder="Phone Number" ref={phoneRef} />
+          <Form.Control
+            type="tel"
+            placeholder="Phone Number"
+            ref={phoneRef}
+            name="phone"
+            value={inputValues.phone}
+            onChange={handleInputChange}
+          />
         </Form.Group>
         <Button variant="dark" type="submit">
           Submit
