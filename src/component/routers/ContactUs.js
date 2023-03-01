bdc7893559e9d47ab7f24474b41f08976db7ca2a
shubhampatil.ani@gmail.com <shubhampatil.ani@gmail.com>
2023-03-01 14:40:51 +0530

    contact us

---
+++ src/component/routers/ContactUs.js
@@ -0,0 +1,11 @@
+import { Form, FormLabel } from "react-bootstrap";
+
+const ContactUs = () => {
+  return (
+    <Form>
+      <FormLabel></FormLabel>
+    </Form>
+  );
+};
+
+export default ContactUs;
